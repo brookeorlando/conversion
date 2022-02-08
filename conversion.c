bb88f4982d92361cbc9908f3c25859dfc423a7f6
brookeorlando <91267635+brookeorlando@users.noreply.github.com>
2022-02-08 09:41:42 -0500

    Create conversion.c

---
+++ conversion.c
@@ -0,0 +1,19 @@
+//pop off SLAYYYYYYY
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+string htob(string a);
+string dtoh(int a);
+int btod(string b);
+
+int main(void);
+{
+    string t = get_string("What data type are you starting with?");
+    string s = get_string("What data type would you like to convert to?");
+    if (strcasecmp(t, "hexadecimal") == 0)
+    {
+        string h = ("Please enter your hexadecimal here: )
+    }
+}
